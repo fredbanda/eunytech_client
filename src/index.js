eb5217051eb5e7bb3ed66282bf6916e3239c78b8
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2024-01-28 15:12:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import {disable}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (Process.env.NODE_ENV === 'production') disableReactDeTools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,10 +15,9 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route  path="/*" element={<App />} />
+          <Route path="/*" element={<App />} />
         </Routes>
       </BrowserRouter>
     </Provider>
-
   </React.StrictMode>
 );
