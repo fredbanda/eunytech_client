e98c6a0c68058a71abbc15f0d2f5dc59d3c0503c
Fred Banda <fredcbanda@hotmail.com>
2024-03-02 12:56:08 +0200

    elll

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') disableReactDeTools()
 
