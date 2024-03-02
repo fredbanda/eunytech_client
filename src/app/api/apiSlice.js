be7ae0eb0aeccebc8e12103bed25ee5a0f40183e
Fred Banda <fredcbanda@hotmail.com>
2024-03-02 12:48:53 +0200

    vercel

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://eunnytech-api.onrender.com',
+    baseUrl: 'https://eunnytech.vercel.app/',
     credentials: 'same-origin',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
